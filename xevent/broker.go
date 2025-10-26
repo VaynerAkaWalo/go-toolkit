c8aa9d45934ae89b4856825d19c26916aed842b6
VaynerAkaWalo <vaynerakawalo@gmail.com>
2025-10-26 23:59:42 +0100

    Broker API

---
+++ xevent/broker.go
@@ -0,0 +1,77 @@
+package xevent
+
+import (
+	"context"
+	"reflect"
+	"sync"
+)
+
+type (
+	Broker struct {
+		channels map[reflect.Type]interface{}
+		mutex    *sync.RWMutex
+	}
+)
+
+func RegisterListener[T any](broker *Broker, ctx context.Context) chan T {
+	eventType := reflect.TypeOf((*T)(nil)).Elem()
+
+	channel := make(chan T)
+
+	broker.mutex.Lock()
+	defer broker.mutex.Unlock()
+
+	rawChannels, ok := broker.channels[eventType]
+	if !ok {
+		panic("cannot process unregistered events")
+	}
+
+	channels, ok := rawChannels.([]chan T)
+
+	channels = append(channels, channel)
+
+	broker.channels[eventType] = channels
+
+	return channel
+}
+
+func RemoveListener[T any](broker *Broker, ctx context.Context, channel chan T) {
+	eventType := reflect.TypeOf((*T)(nil)).Elem()
+
+	broker.mutex.Lock()
+	defer broker.mutex.Unlock()
+
+	rawChannels, ok := broker.channels[eventType]
+	if !ok {
+		panic("cannot process unregistered events")
+	}
+
+	channels, ok := rawChannels.([]chan T)
+
+	close(channel)
+
+	for index, cha := range channels {
+		if cha == channel {
+			broker.channels[eventType] = append(channels[:index], channels[index+1:]...)
+			return
+		}
+	}
+}
+
+func PublishEvent[T any](broker *Broker, ctx context.Context, event T) {
+	eventType := reflect.TypeOf(event)
+
+	broker.mutex.RLock()
+	defer broker.mutex.RUnlock()
+
+	rawChannels, ok := broker.channels[eventType]
+	if !ok {
+		panic("cannot process unregistered events")
+	}
+
+	channels, ok := rawChannels.([]chan T)
+
+	for _, cha := range channels {
+		cha <- event
+	}
+}
